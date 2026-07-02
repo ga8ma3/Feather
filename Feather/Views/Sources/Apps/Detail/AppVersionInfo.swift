5a9112c8ae24b49a0edcd2432e339e781c3a5dd4
ga8ma3 <ga8ma3@users.noreply.github.com>
2026-07-03 06:16:21 +0900

    Fix localized version text build

--- Feather/Views/Sources/Apps/Detail/AppVersionInfo.swift
+++ Feather/Views/Sources/Apps/Detail/AppVersionInfo.swift
@@ -25,7 +25,7 @@ struct AppVersionInfo: View {
 	var body: some View {
 		VStack(alignment: .leading, spacing: 2) {
 			HStack {
-				Text(.localized("Version %@", arguments: version))
+				Text(verbatim: .localized("Version %@", arguments: version))
 					.font(.subheadline)
 					.foregroundStyle(.secondary)
                 
